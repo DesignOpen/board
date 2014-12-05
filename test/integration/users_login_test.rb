d6d96097329857e30024ae9dc9138c953a9cb375
davidweinfls <davidweinfls@163.com>
2014-12-05 08:06:11 -0800

    add test case for user-logged in

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -11,4 +11,24 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, session: { name: @user.email, github: 'password' }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,10 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  # returns true if a test user is logged in
+  def is_logged_in?
+      !session[:user_id].nil?
+  end
+
   # Add more helper methods to be used by all tests here...
 end
