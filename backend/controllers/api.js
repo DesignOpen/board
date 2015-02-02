c502fd5a39dbd41b159f079828856cc795df0cc9
matiasmakitalo <matias.makitalo@student.tut.fi>
2015-02-02 03:29:44 +0200

    Added api controller

---
+++ backend/controllers/api.js
@@ -0,0 +1,9 @@
+
+var post = function(req, res) {
+    res.json({test: 2});
+};
+
+
+module.exports = {
+    post: post
+};
--- backend/routes.js
+++ backend/routes.js
@@ -1,8 +1,12 @@
+var api = require('./controllers/api');
+
 
 function initRoutes(app) {
     app.get('/', function(req, res) {
         res.json({test: 1});
     });
+
+    app.get('/api/v1/post', api.post);
 }
 
 module.exports = {
