b60255ef1283a8fb0b8a97c51b6156b05708f8ed
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2015-02-01 19:17:32 -0800

    Fix methods#

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,14 +1,10 @@
 var userService = require('../services/user-service');
 
+
 function getPosts(req, res, next) {
 
 }
 
-function post(req, res, next) {
-
-}
-
 module.exports = {
-    get: get,
-    post: post
+    getPosts: getPosts
 };
