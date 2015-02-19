095dfea5d19db1d46c79b6591180e97576aa3eba
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2015-02-19 14:55:11 +0200

    Move routes under scripts. Add a few components

---
+++ src/frontend/scripts/components/App.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+
+var App = React.createClass({
+
+    render: function render() {
+        return <h1>APP</h1>;
+    },
+
+    _isLogged: function _isLogged() {
+        return false;
+    }
+});
+
+module.exports = App;
---
+++ src/frontend/scripts/components/NotFound.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var NotFound = React.createClass({
+    render: function render() {
+        return <h1>NotFound</h1>;
+    }
+});
+
+module.exports = NotFound;
