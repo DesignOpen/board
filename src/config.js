ca1bb5a8a2ea1b9ac16ba79c1dafa62e021a7800
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2015-03-08 22:21:35 +0200

    Add api latency emulation option

--- src/config.js
+++ src/config.js
@@ -6,7 +6,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var config = {
     // Absolute path to the directory which contains nodejs views and static
     // image, css and js files
-    serveDir: path.join(__dirname, '../public')
+    serveDir: path.join(__dirname, '../public'),
+
+    // API latency in local development for emulating slow responses.
+    // If null, latency is disabled
+    // This is never applied in production
+    apiLatency: null
 };
 
 module.exports = config;
