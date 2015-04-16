01373c21555178c55005d686eaae7f088cc115e4
matiasmakitalo <matias.makitalo@student.tut.fi>
2015-04-16 14:19:41 +0300

    Add markdown support to post content

--- src/frontend/scripts/components/PostContent.jsx
+++ src/frontend/scripts/components/PostContent.jsx
@@ -1,12 +1,14 @@
 var React = require('react');
+var marked = require('react-marked');
 
 var PostContent = React.createClass({
     render: function render() {
         var post = this.props.post;
+        var renderedContent = marked(post.content);
 
         return (
             <div className="post-content">
-                <p>{post.content}</p>
+                <p>{renderedContent}</p>
             </div>
         );
     }
